387e1837fb10da5653a687a25456453029924eb9
WonbaeLee <gloomygenltmena@gmail.com>
2021-10-15 23:57:11 +0900

    add test directory

---
+++ test/craco.config.js
@@ -0,0 +1,31 @@
+const imageOptimizer = require('craco-image-optimizer-plugin');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: imageOptimizer,
+      // image-webpack-plugin options
+      options: {
+        mozjpeg: {
+          progressive: true,
+          quality: 65,
+        },
+        // optipng.enabled: false will disable optipng
+        optipng: {
+          enabled: false,
+        },
+        pngquant: {
+          quality: [0.65, 0.9],
+          speed: 4,
+        },
+        gifsicle: {
+          interlaced: false,
+        },
+        // the webp option will enable WEBP
+        webp: {
+          quality: 75,
+        },
+      },
+    },
+  ],
+};
---
+++ test/src/App.jsx
@@ -0,0 +1,11 @@
+import BusinessJPG from './image/business.jpg';
+
+const App = () => {
+  return (
+    <div>
+      <img src={BusinessJPG} alt="test" />
+    </div>
+  );
+};
+
+export default App;
